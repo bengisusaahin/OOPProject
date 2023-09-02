13028a39ea1b96067fdc6716e84cca317b194526
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-09-02 15:10:48 +0300

    Encapsulation

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
 
         User myUser = new User("Atil", "Instructor");
         System.out.println(myUser.name);
+
+        //Encapsulation
+        Musician james = new Musician("James","Guitar",50);
+        //james.age = 60;
+        //System.out.println(james.age);
+        System.out.println(james.getName());
+        james.setAge(60,"Atil");
+        System.out.println(james.getAge());
     }
 }
